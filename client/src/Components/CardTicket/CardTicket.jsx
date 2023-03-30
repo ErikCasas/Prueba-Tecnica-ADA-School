2d5fbe1b745646624afded44b08a4e187cb1a33a
ErikCasas <santicasas667@gmail.com>
2023-03-29 22:48:30 -0500

    Home

--- client/src/Components/CardTicket/CardTicket.jsx
+++ client/src/Components/CardTicket/CardTicket.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import airplane from '../../svg/airplane.svg';
 
 const CardTicket = ({
   name,
@@ -12,27 +14,62 @@ const CardTicket = ({
 }) => {
   return (
     <>
-      <div className="card ticket-card">
+      <Container className="card ticket-card">
         <div className="card-body">
           <h5 className="card-title ticket-title">{name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted ticket-destination">
-            available {available}
+          <h6 className="card-subtitle mb-2 text-muted ticket-destination d-flex">
+            available 
+            <TextInfo> 
+              {available}
+              </TextInfo>
           </h6>
-          <p className="card-text ticket-name">
-            from {origin} to {detinity}
+          <p className="card-text ticket-name d-flex m-0">
+            from 
+            <TextInfo>
+            {origin}
+            </TextInfo>
+             to 
+            <TextInfo>
+             {detinity}
+            </TextInfo>
+
           </p>
-          <p className="card-text ticket-date">Departure Date: {date}</p>
+          <p className="card-text ticket-date d-flex m-0">Departure Date: 
+          <TextInfo>
+          {date}
+          </TextInfo>
+          </p>
           {render ? (
+            <ConatinerButton>
             <Link to={`ticket/${id}`}>
               <button type="button" className="btn btn-primary m5">
                 Get
+                <img src={airplane} alt="airplane" width={30} />
               </button>
             </Link>
+            </ConatinerButton>
           ) : null}
         </div>
-      </div>
+      </Container>
     </>
   );
 };
 
+const Container = styled.div`
+  border: dashed red 5px;
+  border-radius: 10px;
+  box-shadow: 0px 10px 10px;
+`;
+
+const TextInfo = styled.p`
+  font-weight: bold;
+  margin-left: 8px;
+  margin-right: 8px;
+`;
+
+const ConatinerButton = styled.div`
+display: flex;
+justify-content: flex-end;
+padding-right: 30px;
+`
 export default CardTicket;
--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -1,36 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAuthToken } from '../../api/axiosConfig';
 import CardTicket from '../../Components/CardTicket/CardTicket';
 import { getAllTickets } from '../../Store/Slices/Ticket/Thunk';
+import styled from 'styled-components';
 
 const Home = () => {
   const dispatch = useDispatch();
+
+  const [numberOfTickets, setnumberOfTickets] = useState(12);
+
   const token = localStorage.getItem('token');
+
+  const allTickets = useSelector((state) => state.ticket.allTickets.tickets);
+
+  const Tickets = allTickets?.slice(0, numberOfTickets);
+
+  const moreTickets = () => {
+    setnumberOfTickets(numberOfTickets + 4);
+  };
 
   useEffect(() => {
     setAuthToken(); // actualiza el token en los encabezados de la solicitud
     dispatch(getAllTickets());
   }, [dispatch, token]);
 
-  const allTickets = useSelector((state) => state.ticket.allTickets.tickets);
-
   return (
     <>
-      {allTickets?.map((el) => (
-        <CardTicket
-          key={el._id}
-          id={el._id}
-          detinity={el.destiny}
-          origin={el.origin}
-          date={el.date}
-          available={el.quantityAvailable}
-          name={el.name}
-          render={true}
-        />
-      ))}
+      <Container>
+        {Tickets?.map((el) => (
+          <CardTicket
+            et
+            key={el._id}
+            id={el._id}
+            detinity={el.destiny}
+            origin={el.origin}
+            date={el.date}
+            available={el.quantityAvailable}
+            name={el.name}
+            render={true}
+          />
+        ))}
+      </Container>
+      <ContainerButton>
+        <button type="button" className="btn btn-success btn-lg" onClick={moreTickets}>
+          {numberOfTickets <= allTickets?.length
+            ? 'load more'
+            : 'No more (⩾﹏⩽)'}
+        </button>
+      </ContainerButton>
     </>
   );
 };
 
+const Container = styled.div`
+  background-color: #0193cc;
+  height: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  grid-gap: 20px;
+  justify-content: center;
+  padding: 40px 50px;
+  @media (max-width: 767px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+const ContainerButton = styled.div`
+  background-color: #0193cc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
 export default Home;
